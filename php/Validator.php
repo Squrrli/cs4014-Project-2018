1c9fa27c45508c7da09a6b6a71be7f98413746e9
Maniac <danylovarushchyk@gmail.com>
2018-04-07 00:59:27 +0100

    Validator remake

--- php/Validator.php
+++ php/Validator.php
@@ -3,67 +3,101 @@ class Validator
 {
     public function validate_id($value)
     {
-        $id_lower_limit = 0;
-        $id_upper_limit = 10000;
-        return (is_numeric($value)) && ($id_lower_limit < $value && $value < $id_upper_limit);
+        $value_num = $value + 0;
+        $value_num .= "";
+        # compare string with string of digits from initial string
+        if ($value_num != $value)
+        {
+            return 0;
+        }
+        if ($value >= 0 && $value <= 9999999999)
+        {
+            return 1;
+        }
+        return 0;
     }
     public function validate_date($value)
     {
         $value_list = explode("-", $value);
-        # TODO move numbers here <--
         if (sizeof($value_list) != 3)
         {
-            return false;
+            return 0;
         }
         if ($value_list[2] > 31 || $value_list[2] < 0)
         {
-            # TODO different amount of days for every month
-            return false;
+            return 0;
         }
         if ($value_list[1] > 12 || $value_list[1] < 0)
         {
-            return false;
+            return 0;
         }
-        if ($value_list[0] > 2020 || $value_list[0] < 1900)
+        if ($value_list[0] > 3000 || $value_list[0] < 1000)
         {
-            # TODO change to current year
-            return false;
+            return 0;
         }
-        return true;
+        return 1;
 
     }
     public function validate_name($value)
     {
-        $pattern = "/^[a-zA-Z]{1,50}$/";
-        return boolval(preg_match($pattern, $value));
+        if (strlen($value) <= 50 && strlen($value) > 0)
+        {
+            return 1;
+        }
+        return 0;
     }
     public function validate_type($value)
     {
-        $pattern = "/^[a-zA-Z]{1,20}$/";
-        return boolval(preg_match($pattern, $value));
+        if (strlen($value) <= 20 && strlen($value) > 0)
+        {
+            return 1;
+        }
+        return 0;
     }
     public function validate_admin($value)
     {
-        return is_bool($value);
+        if ($value === "false" || $value === "true")
+        {
+            return 1;
+        }
+        return 0;
     }
     public function validate_timestamp($value)
     {
-        # TODO complete function!
-        return true;
+        #  TODO add later
+        # we currently don't need timestamp so it's not here:)
+        return 1;
     }
     public function validate_description($value)
     {
-        $pattern = "/^.{1,1000}$/";
-        return boolval(preg_match($pattern, $value));
+        if (strlen($value) <= 1000 && strlen($value) > 0)
+        {
+            return 1;
+        }
+        return 0;
     }
     public function validate_text($value)
     {
-        $pattern = "/^.{1,5000}$/";
-        return boolval(preg_match($pattern, $value));
+        if (strlen($value) <= 5000 && strlen($value) > 0)
+        {
+            return 1;
+        }
+        return 0;
     }
     public function validate_code($value)
     {
-        return (is_numeric($value)) && (0 < $value && $value < 10);
+        $value_num = $value + 0;
+        $value_num .= "";
+        # compare string with string of digits from initial string
+        if ($value_num != $value)
+        {
+            return 0;
+        }
+        if ($value >= 0 && $value <= 9)
+        {
+            return 1;
+        }
+        return 0;
     }
     public function validate_email($value)
     {
@@ -72,7 +106,20 @@ class Validator
     }
     public function validate_password($value)
     {
-        $pattern = "/.{1,20}/";
-        return boolval(preg_match($pattern, $value));
+        if (strlen($value) <= 50 && strlen($value) > 0)
+        {
+            return 1;
+        }
+        return 0;
     }
 }
+# id: number(int or string) [0..999999999]
+# date: string [1000..3000]-[0..12]-[0..31]
+# name: any string of 1..50 length
+# type: any string of 1..20 length
+# admin/bool: string in array ["true", "false"]
+# description: any string of 1..1000 length
+# text: any string of 1..5000 length
+# id: number(int or string) [0..9]
+# email: string letter/number{1..50}@letter/number{1..q0}.letter/number{2..INF}";
+# password: any string of 1..50 length
